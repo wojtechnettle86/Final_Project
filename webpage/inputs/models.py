a65086c40972226bbf1a4f8c1cf1f209d0de7cc9
wojtechnettle86 <wojtech.nettle@gmail.com>
2024-10-28 19:35:20 +0100

    Inputs

---
+++ webpage/inputs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Employee(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  email = models.CharField(max_length=100, null=True)
+
+  def __str__(self):
+    return f"{self.firstname}"
--- webpage/webpage/urls.py
+++ webpage/webpage/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('inputs/', include('inputs.urls')),
     path('admin/', admin.site.urls),
 ]
