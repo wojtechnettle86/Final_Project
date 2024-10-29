af5d75a43bac746cb2033441de0389c61d0862cb
wojtechnettle86 <wojtech.nettle@gmail.com>
2024-10-29 22:54:19 +0100

    Update settings.py

--- webpage/webpage/settings.py
+++ webpage/webpage/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/home/wojtechnettle/wojtechnettle.pythonanywhere.com/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'/home/wojtechnettle/wojtechnettle.pythonanywhere.com/static')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
