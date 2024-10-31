7689fb28fff52d805891f6c5cace57cce5c678a6
wojtechnettle86 <wojtech.nettle@gmail.com>
2024-10-31 21:41:09 +0100

    validation

--- webpage/core/static/js/script.js
+++ webpage/core/static/js/script.js
@@ -6,3 +6,27 @@ element.classList.add("form-control");
 
 var element = document.getElementById("id_password2");
 element.classList.add("form-control");
+
+function checkPassword(form) {
+    password1 = form.password1.value;
+    password2 = form.password2.value;
+
+    if (password1 == '')
+        alert("Vyplňte prosím heslo");
+
+    else if (password1.length < 8)
+        alert("Heslo musí mít nejméně 8 znaků");
+
+    else if (password2 == '')
+        alert("Zadejte stejné heslo do druhého pole");
+  
+    else if (password1 != password2) {
+        alert("\nHesla se neshodují, zkuste to znovu")
+        return false;
+    }
+
+    else {
+        alert("Registrace proběhla v pořádku")
+        return true;
+    }
+}
